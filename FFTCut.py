a7cfb3edd6dbbd2a5bf2e131f62fc1efbbd91e48
Simon Herron <simonherron4@gmail.com>
2022-01-01 23:40:09 -0500

    updated for remote paths. transients all good (I think). Quiet and high notes end too soon.

--- FFTCut.py
+++ FFTCut.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
 import matplotlib.pyplot as plt
-from scipy.fft import rfft, fftfreq
-from scipy.signal import flattop, hamming
+#from scipy.fft import rfft, fftfreq
+from scipy.signal import hamming
 from soundfile import SoundFile
+import soundfile as sf
 import numpy as np
+import os
 
-noteLines = open("/Users/simonherron/Sampler/Piano/PianoNoPedal.csv").read().split("\n")
+homeDir = os.path.dirname(__file__)
+
+
+noteLines = open(homeDir + "/Piano/PianoNoPedalV1.csv").read().split("\n")
 
 print("Importing Wave")
-waveFile = SoundFile("/Users/simonherron/Sampler/Piano/08/Spaced Omni_08.wav", mode = "rb")
+waveFile = SoundFile(homeDir + "/Piano/08/Spaced Omni_08.wav", mode = "rb")
 waveFile.seek(0)
-theWave = waveFile.read()[:,0]
+stereo = waveFile.read()
+theWave = stereo[:,0]
+otherChannel = stereo[:,1]
 
 print("Cutting")
 
@@ -31,9 +38,9 @@ def findTransientAndSilence(data, sampleRate):
     bLMean = np.mean(baseLine)
     bLSD = np.std(baseLine)
     
-    plt.axhline(y = bLMean, linestyle = "--", color = "purple")
-    plt.axhline(y = bLMean + 2*bLSD, linestyle = "--", color = "purple")
-    plt.axhline(y = bLMean + 3*bLSD, linestyle = "--", color = "purple")
+#    plt.axhline(y = bLMean, linestyle = "--", color = "purple")
+#    plt.axhline(y = bLMean + 2*bLSD, linestyle = "--", color = "purple")
+#    plt.axhline(y = bLMean + 3*bLSD, linestyle = "--", color = "purple")
 #    plt.axhline(y = bLMean + 4*bLSD, linestyle = "--", color = "purple")
     
     for i in range(len(data)):
@@ -46,25 +53,23 @@ def findTransientAndSilence(data, sampleRate):
                 
     transient = i
     
-    testMeanX = []
-    testMeanY = []
-    testSDX = []
-    testSDY = []
+#    testMeanX = []
+#    testMeanY = []
+#    testSDX = []
+#    testSDY = []
     
     for k in range(transient + 500, len(data)):
         if ((data[k] - bLMean) / bLSD) < 1:
             block = data[k : (k+200)]
             
             m = np.mean(block)
-            testMeanX.append(k)
-            testMeanY.append(m)
-            
+#            testMeanX.append(k)
+#            testMeanY.append(m)
             if m < bLMean + 0.2 * bLSD:
                 
                 s = np.std(block)
-                testSDX.append(k)
-                testSDY.append(s)
-            
+#                testSDX.append(k)
+#                testSDY.append(s)
                 if np.std(block) < 1.1 * bLSD:
                     break
     
@@ -74,19 +79,31 @@ def findTransientAndSilence(data, sampleRate):
     return (transient, k)
 
 def refineTransient(trans, binSize, n, bLVol):
-    bins = np.linspace(trans, trans - 32 * 30, 29).astype(int)
+    binSize = 2**5
     
-    print(bLVol)
+    i = trans
+    block = theWave[(i - binSize) : i]
+    t = vol(block)
+    cutoff = trans - 4000
     
-    for i in bins:
-        block = theWave[(i - 32) : i]
+    while (t > 1.1 * bLVol) and (i > cutoff): # i.e. no find yet
+        i -= binSize
+        block = theWave[(i - binSize) : i]
         t = vol(block)
-        print(t)
-        if t < 1.1 * bLVol:
-            return i
-    
-    print("no find")
-    return trans
+
+    return i
+#
+##    bins = np.linspace(trans, trans - 32 * 30, 29).astype(int)
+##    print(bLVol)
+#
+#    for i in bins:
+#        block = theWave[(i - 32) : i]
+##        print(t)
+#        if t < 1.1 * bLVol:
+#            return i
+#
+#    print("no find")
+#    return trans
 
 
 def findZero(startFrame, direction):
@@ -101,20 +118,13 @@ def findZero(startFrame, direction):
     
     return nextFrame + change
     
-#    block = theWave[(startFrame - 100) : (startFrame + 100)]
-#
-#    print(block)
-#    print(np.min(block))
-#
-#    plt.plot(block, ".")
-#    plt.show()
 
 def freqData(startFrame, endFrame, targetFreq):
-    binSize = 2**6 # 2*86
-    nfft = 2**10
+    binSize = 2**6 # 2**6
+    nfft = 2**13
     
     xbins = np.arange(startFrame, endFrame, binSize)
-    harmonics = 3
+    harmonics = 6
     freqs = targetFreq * np.arange(1, harmonics + 1)
     
     window = hamming(nfft, sym = False)
@@ -143,9 +153,16 @@ def cut():
     firstPitch = 23.5
     offSet = firstPitch - float(noteLines[1].split(",")[6])
     
-    for noteIndex in [128, 482, 488]: #E2
+    outputCSV = ["Velocity,Repitition,Sustain,MidiPitch,Pitch,Time,Delete,Duration,Max,TransientFrame,SilenceFrame"]
+    outputPath = homeDir + "/Piano/SpacedOmni_Kontakt/"
+    velocityLayers = ["pp", "mp", "f"]
+    
+    for noteIndex in range(1, len(noteLines)-1): #[2, 7, 128]: # #[128, 482, 488]: #E2
         velocity, repitition, sustain, midiPitch, pitch, time, delete = noteLines[noteIndex].split(",")[1:-4]
+        noteComponents = [velocity, repitition, sustain, midiPitch, pitch, time, delete]
+        
         if delete != "d":
+            # DFT
             startFrame = int((float(time) + offSet - 1) * 48000)
             endTime = noteLines[noteIndex + 1].split(",")[6] # start time of following pitch
             endFrame = (float(endTime) + offSet + 1) * 48000
@@ -156,6 +173,7 @@ def cut():
                         
             trans, silence = findTransientAndSilence(newSignal, 48000 // fftBinSize)
             
+            # Refine
             baseLineEndFrame = round(48000 * 0.4) # want 0.4 secs of base line
             rawBaseLine = theWave[startFrame : (startFrame + baseLineEndFrame)]
             rawBLVol = vol(rawBaseLine)
@@ -166,23 +184,33 @@ def cut():
             
             silenceFrame = (startFrame + silence * fftBinSize) + nfft
             silenceFrame2 = findZero(silenceFrame, "f")
-
+            
             # Calculate Meta Data
             theSize = silenceFrame2 - transFrame3
             duration = theDuration = round(theSize / 48000, 2)
             
+            maxL = np.max(theWave[transFrame3 : (transFrame3 + 20000)])
+            maxR = np.max(otherChannel[transFrame3 : (transFrame3 + 20000)])
+            theMax = np.max([maxL, maxR])
             
-
-
+            noteComponents.extend([str(duration), str(theMax), str(transFrame3), str(silenceFrame2)])
+            
+            # Write File
+            nameComponents = ["Piano", "SpacedOmni", "NoPedal", midiPitch, pitch, str(velocityLayers.index(velocity) + 1), velocity, repitition]
+            filePath = outputPath + velocity + "_" + repitition + "/" + "_".join(nameComponents) + ".wav"
+            print(" ".join(nameComponents))
+            toWrite = stereo[transFrame3 : silenceFrame2, :]
+            sf.write(filePath, toWrite, 48000)
+        
+       
+            
+            
 #            print("Plotting")
-#            plt.plot(newSignal, ".", zorder = 0)
-
-#            plt.axvline(x = trans, linestyle = "--", color = "red")
-##            plt.axvline(x = (48000 // fftBinSize) * 0.4, linestyle = "--", color = "green")
-##            plt.axvline(x = silence, linestyle = "--", color = "orange")
-#            plt.scatter(trans, newSignal[trans], color = "red", zorder = 2)
-#            plt.show()
+#            print(transFrame)
+#            print(transFrame2)
+#            print(transFrame3)
 #
+#            print(silenceFrame2)
 #
 #            tf = int(transFrame)
 #            tf2 = int(transFrame2)
@@ -192,11 +220,24 @@ def cut():
 #            plt.axvline(x = tf2, linestyle = "--", color = "black")
 #            plt.axvline(x = transFrame3, linestyle = "--", color = "purple")
 #            plt.axvline(x = startFrame + baseLineEndFrame, linestyle = "--", color = "green")
-#
 #            plt.axhline(y = 0, color = "black")
-#
 #            plt.show()
+#
+#
+#            plt.plot(newSignal, ".", zorder = 0)
+#            plt.axvline(x = trans, linestyle = "--", color = "red")
+#            plt.axvline(x = (48000 // fftBinSize) * 0.4, linestyle = "--", color = "green")
+#            plt.axvline(x = silence, linestyle = "--", color = "orange")
+#            plt.scatter(trans, newSignal[trans], color = "red", zorder = 2)
+#            plt.show()
+
+
             
+        outputCSV.append(",".join(noteComponents))
+    
+    outputCSVFile = open(homeDir + "/Piano/PianoNoPedalV2.csv", "wt")
+    outputCSVFile.write("\n".join(outputCSV))
+    outputCSVFile.close()
    
 
 
